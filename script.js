30292850b0462bec613faeda2a592b761d727dde
Gabriel Artur <gabrielgslm@gmail.com>
2022-06-07 19:39:47 -0300

    requisito 3 completo

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const entrar = document.querySelector(".botao-login");
-entrar.addEventListener("click", function formulario() {
-    const login = "tryber@teste.com";
-    const senha = "123456";
-    const usuario = document.querySelector("#email");
-    const password = document.querySelector("#password");
-        if (usuario.value === login && password.value === senha) {
-            return window.alert("Olá, Tryber!");
-        } else if (usuario.value != login && password.value != senha) {
-            return window.alert("Email ou senha inválidos");
-            }
+entrar.addEventListener("click", function () {
+  const login = "tryber@teste.com";
+  const senha = "123456";
+  const usuario = document.querySelector("#email");
+  const password = document.querySelector("#password");
+  if (usuario.value === login && password.value === senha) {
+    return window.alert("Olá, Tryber!");
+  }
+  return window.alert("Email ou senha inválidos");
+
 });
 
-formulario();
+
