27914976474bceb4dd7d555f97871851c5f7b576
Gabriel Artur <gabrielgslm@gmail.com>
2022-06-09 20:51:10 -0300

    adiciona comentários no js

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-const entrar = document.querySelector(".botao-login");
-entrar.addEventListener("click", function () {
-  const login = "tryber@teste.com";
-  const senha = "123456";
-  const usuario = document.querySelector("#email");
-  const password = document.querySelector("#password");
-  if (usuario.value === login && password.value === senha) {
-    return window.alert("Olá, Tryber!");
+const entrar = document.querySelector(".botao-login"); //cria constante para atribuir o valor do botão login
+entrar.addEventListener("click", function () { //cria evento de click para o botão 
+  const login = "tryber@teste.com"; //cria variável com o valor correto do email para ser verificado 
+  const senha = "123456"; //cria variável com valor correto da senha para ser verificada 
+  const usuario = document.querySelector("#email"); //cria contante para atribuir o valor do campo email 
+  const password = document.querySelector("#password"); //cria contante para atribuir o valor do campo senha 
+  if (usuario.value === login && password.value === senha) { // cria condição para verificar se oq foi digitado no email tem o mesmo valor do email/senha 'corretos'
+    return window.alert("Olá, Tryber!"); //retorna uma janela com a mensagem 
   }
-  return window.alert("Email ou senha inválidos");
+  return window.alert("Email ou senha inválidos"); //retorna a janela com mensagem 
 });
+
+
 
 
