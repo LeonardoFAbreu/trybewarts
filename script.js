54b14e9ec5db750d9713647ae0ee2d481397ceb4
Leonardo Abreu <leonardofabreu@hotmail.com>
2022-06-09 20:43:17 -0300

    Adiciona comentário

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
+/* eslint-disable no-else-return */
 const entrar = document.querySelector(".botao-login");
+// eslint-disable-next-line prefer-arrow-callback
 entrar.addEventListener("click", function formulario() {
-    const login = "tryber@teste.com";
-    const senha = "123456";
-    const usuario = document.querySelector("#email");
-    const password = document.querySelector("#password");
-        if (usuario.value === login && password.value === senha) {
-            return window.alert("Olá, Tryber!");
-        } else if (usuario.value != login && password.value != senha) {
-            return window.alert("Email ou senha inválidos");
-            }
+  const login = "tryber@teste.com";
+  const senha = "123456";
+  const usuario = document.querySelector("#email");
+  const password = document.querySelector("#password");
+  if (usuario.value === login && password.value === senha) {
+    return window.alert("Olá, Tryber!");
+  } else if (usuario.value != login && password.value != senha) {
+    return window.alert("Email ou senha inválidos");
+  }
 });
 
 formulario();
